159bf3ae14f90a07bec4ec58ced95a0d75dd998d
Danish Alfattah <danishalfattah25@gmail.com>
2025-07-10 19:27:32 +0700

    feat: add contact page with card and hour entry components

--- src/app/kontak/page.tsx
+++ src/app/kontak/page.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import { Playfair_Display } from "next/font/google";
 import { Poppins } from "next/font/google";
 import { Card } from "./components/card";
-import { Clock } from "lucide-react";
 
 import { Phone, Mail, Instagram } from "lucide-react";
 import { HourEntry } from "./components/hour-entry";
@@ -21,11 +20,23 @@ export default function KontakPage() {
   return (
     <main className="min-h-screen bg-white">
       {/* Hero Section */}
-      <section className="relative h-[350px] md:h-[400px] w-full flex items-center justify-center bg-cover bg-center" style={{backgroundImage: 'url(/landing-page.png)'}}>
+      <section
+        className="relative h-[350px] md:h-[400px] w-full flex items-center justify-center bg-cover bg-center"
+        style={{ backgroundImage: "url(/landing-page.png)" }}
+      >
         <div className="absolute inset-0 bg-black/40" />
         <div className="relative z-10 text-center text-white">
-          <h1 className={`${playfair.className} text-4xl md:text-5xl font-medium mb-4 tracking-[9px]`}>KONTAK</h1>
-          <p className={`${poppins.className} text-lg md:text-xl font-thin max-w-2xl mx-auto`}>Layanan Desa Slamparejo dirancang untuk memberikan kemudahan, kenyamanan, dan kejelasan dalam setiap proses pelayanan.</p>
+          <h1
+            className={`${playfair.className} text-4xl md:text-5xl font-medium mb-4 tracking-[9px]`}
+          >
+            KONTAK
+          </h1>
+          <p
+            className={`${poppins.className} text-lg md:text-xl font-thin max-w-2xl mx-auto`}
+          >
+            Layanan Desa Slamparejo dirancang untuk memberikan kemudahan,
+            kenyamanan, dan kejelasan dalam setiap proses pelayanan.
+          </p>
         </div>
       </section>
 
@@ -44,14 +55,16 @@ export default function KontakPage() {
             <h2
               className={`${playfair.className} text-white text-2xl md:text-8xl font-normal tracking-[1.5px] mb-4`}
             >
-                Kontak<br></br>Desa
+              Kontak<br></br>Desa
             </h2>
           </div>
           <div className="md:w-1/2 flex items-center ">
             <p
               className={`${poppins.className}  text-white text-sm md:text-lg font-normal tracking-wider`}
             >
-                Hubungi kami melalui informasi kontak di bawah ini jika anda memiliki pertanyaan atau permohonan untuk Pemerintah Desa Slamparejo.
+              Hubungi kami melalui informasi kontak di bawah ini jika anda
+              memiliki pertanyaan atau permohonan untuk Pemerintah Desa
+              Slamparejo.
             </p>
           </div>
         </div>
@@ -65,24 +78,27 @@ export default function KontakPage() {
               description="Kirim email untuk pertanyaan atau permohonan resmi"
               contactInfo="desa.slamparejo@gmail.com"
               link="mailto:desa.slamparejo@gmail.com"
-              buttonText="Kirim Email">
-                <Mail />
+              buttonText="Kirim Email"
+            >
+              <Mail />
             </Card>
-            <Card 
+            <Card
               title="Telepon Kantor"
               description="Hubungi langsung untuk informasi terpercaya dengan cepat"
               contactInfo="(+62)895-5450-6045"
               link="tel:+6289554506045"
-              buttonText="Hubungi Sekarang">
-                <Phone />
+              buttonText="Hubungi Sekarang"
+            >
+              <Phone />
             </Card>
             <Card
               title="Instagram"
               description="Ikuti Kegiatan dan Berita Terbaru dari Desa Slamparejo"
               contactInfo="@desa_slamparejo"
               link="https://www.instagram.com/desa_slamparejo/"
-              buttonText="Kunjungi Instagram">
-                <Instagram />
+              buttonText="Kunjungi Instagram"
+            >
+              <Instagram />
             </Card>
           </div>
         </div>
@@ -90,10 +106,14 @@ export default function KontakPage() {
       <section className="bg-white p-[37px]">
         <div className="ml-[56px]">
           <div className="border-b border-black py-5 w-fit">
-            <h1 className={`${playfair.className} font-normal text-5xl`}>Jam Operasional</h1>
+            <h1 className={`${playfair.className} font-normal text-5xl`}>
+              Jam Operasional
+            </h1>
           </div>
           <div className="h-[78px] my-[13px]">
-            <p className={`${poppins.className}`}>Kantor Desa Slamparejo melayani masyarakat pada jam berikut</p>
+            <p className={`${poppins.className}`}>
+              Kantor Desa Slamparejo melayani masyarakat pada jam berikut
+            </p>
           </div>
           <div className="grid grid-cols-2 grid-rows-2 mx-[137px] rounded-xl shadow-xl p-[37px] gap-y-[36px] gap-x-[134px]">
             <HourEntry
