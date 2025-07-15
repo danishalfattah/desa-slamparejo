1fcb860de96e233419794249784e5b86a5129cc8
Danish Alfattah <danishalfattah25@gmail.com>
2025-07-15 17:58:15 +0700

    feat: add Launching component to Beranda page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,12 @@
 import Faq from "@/features/landing/components/faq";
 import Hero from "@/features/landing/components/hero";
+import Launching from "@/features/landing/components/launching";
 
 export default function Beranda() {
   return (
     <>
       <Hero />
+      <Launching />
       <Faq />
     </>
   );
