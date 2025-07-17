e9e9a6684bb8642a41cfc3e4b6e8f07a24a05a50
kaneysha <itzkaney@gmail.com>
2025-07-17 21:50:45 +0700

    benerin page usaha

--- src/features/usaha-desa/desc.tsx
+++ src/features/usaha-desa/desc.tsx
@@ -13,18 +13,17 @@ const poppins = Poppins({
 
 export default function DescUsaha() {
   return (
-    <section className="px-5 py-10 bg-[#F9FCFC]">
-      <div className="max-w-6xl mx-auto">
-        <h2
-          className={`${playfair.className} text-2xl md:text-4xl text-black border-b border-gray-400 inline-block pb-2`}
-        >
-          UMKM Desa Slamparejo
-        </h2>
+    <section className="px-4 py-10 pb-2 bg-[#F9FCFC]">
+      <div className="p-[12px] max-w-screen-xl mx-auto">
+        <div className="border-b border-black pb-4 w-fit">
+          <h1 className={`${playfair.className} font-normal text-4xl md:text-5xl`}>
+            UMKM Desa Slamparejo
+          </h1>
+        </div>
         <p
-          className={`${poppins.className} text-base md:text-lg text-gray-800 mt-4 leading-relaxed max-w-3xl`}
+          className={`${poppins.className} text-base md:text-lg text-gray-800 mt-4 leading-relaxed`}
         >
-          Setiap jengkal tanah, setiap tarikan napas warga, adalah bagian dari cerita besar yang hidup.
-          Inilah Slamparejo, desa yang tumbuh dalam makna.
+          Setiap jengkal tanah, setiap tarikan napas warga, adalah bagian dari cerita besar yang hidup. Inilah Slamparejo, desa yang tumbuh dalam makna.
         </p>
       </div>
     </section>
