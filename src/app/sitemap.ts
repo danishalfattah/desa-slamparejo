7790ac06212db600996b89abf148981317022777
Danish Alfattah <danishalfattah25@gmail.com>
2025-07-29 18:51:09 +0700

    feat: update base URL in sitemap to reflect the correct domain

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://desa-slamparejo.vercel.app';
+  const baseUrl = 'https://desaslamparejo.id';
 
   // Halaman statis
   const staticRoutes = [
